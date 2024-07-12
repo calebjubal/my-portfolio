82d29f50e039e8ec2bad74024cec875689bca602
calebjubal <calebjubal@outlook.com>
2024-07-12 16:59:40 +0530

    link update

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,49 +1,64 @@
-import React, { useState, useRef } from 'react' 
+import React, { useState, useRef } from 'react';
 import CustomHook from './CustomHook';
 
 function Contacts() {
-const [listContacts] = useState([
-  {
-    title: 'Phone Number',
-    value: '+91 77039 52970'
-  },{
-    title: 'Email',
-    value: 'chandrasekarcaleb@gmail.com'
-  },{
-    title: 'Instagram',
-    value: '@dj_calebrocks'
-  },{
-    title: 'LinkedIn',
-    value: 'https://www.linkedin.com/in/caleb-chandrasekar-b8521026b/'
-  },{
-    title: 'Github',
-    value: 'https://github.com/calebjubal'
-  }
-])
-const divs = useRef([]);
-const scrollTab = useRef();
-CustomHook(scrollTab, divs);
+  const [listContacts] = useState([
+    {
+      title: 'Phone Number',
+      value: '+91 77039 52970',
+      link: null
+    },
+    {
+      title: 'Email',
+      value: 'chandrasekarcaleb@gmail.com',
+      link: 'mailto:chandrasekarcaleb@gmail.com'
+    },
+    {
+      title: 'Instagram',
+      value: '@dj_calebrocks',
+      link: 'https://www.instagram.com/dj_calebrocks'
+    },
+    {
+      title: 'LinkedIn',
+      value: 'https://www.linkedin.com/in/caleb-chandrasekar-b8521026b/',
+      link: 'https://www.linkedin.com/in/caleb-chandrasekar-b8521026b/'
+    },
+    {
+      title: 'Github',
+      value: 'https://github.com/calebjubal',
+      link: 'https://github.com/calebjubal'
+    }
+  ]);
+  
+  const divs = useRef([]);
+  const scrollTab = useRef();
+  CustomHook(scrollTab, divs);
 
   return (
     <section className='contacts' ref={scrollTab}>
-       <div className="title" ref={(el) => el && divs.current.push(el)}>
+      <div className="title" ref={(el) => el && divs.current.push(el)}>
         These are my Contacts
-       </div>
-       <div className="des" ref={(el) => el && divs.current.push(el)}>
+      </div>
+      <div className="des" ref={(el) => el && divs.current.push(el)}>
         {/* 20 */}
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis quae veniam amet nesciunt voluptatibus quis consectetur consequatur quisquam harum.
-       </div>
-       <div className="list"  ref={(el) => el && divs.current.push(el)}>
+      </div>
+      <div className="list" ref={(el) => el && divs.current.push(el)}>
         {
-          listContacts.map((value, key) => (
+          listContacts.map((contact, key) => (
             <div className='item' key={key}>
-              <h3>{value.title}</h3>
-              <div>{value.value}</div>
+              <h3>{contact.title}</h3>
+              {
+                contact.link ? 
+                <a href={contact.link} target="_blank" rel="noopener noreferrer">{contact.value}</a> :
+                <p>{contact.value}</p>
+              }
             </div>
           ))
         }
-       </div>
+      </div>
     </section>
-  )
+  );
 }
-export default Contacts
+
+export default Contacts;
